7126da3a0845f9c447f2ad046b60695725785a65
NasikyeCynthia <cynthianasikye@gmail.com>
2024-10-17 20:43:16 +0300

    Add files via upload

---
+++ A3.js
@@ -0,0 +1,3 @@
+//const is used to create constant variable
+//var is used to create variables that can be changed and redeclared
+//let is used to create variables that can be changed but cannot be redeclared
