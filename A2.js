3b76fdc8fb69539e666ee9d3d32945e708dfd67a
NasikyeCynthia <cynthianasikye@gmail.com>
2024-10-17 20:42:51 +0300

    Add files via upload

---
+++ A2.js
@@ -0,0 +1,7 @@
+// We assign values to the variables country and population
+let country = "Uganda";
+let population = 500000;
+// The variable is made constant therefore cannot be redeclared
+const year = 2024;
+// The output is printed
+console.log(`The population of ${country} in ${year} is ${population}`);
